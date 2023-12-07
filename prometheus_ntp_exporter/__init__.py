fc1cef1835e07377e02ef28f58488d8f04df925b
niels <n.reuter@fz-juelich.de>
2023-12-07 12:44:18 +0100

    Add version string

--- prometheus_ntp_exporter/__init__.py
+++ prometheus_ntp_exporter/__init__.py
@@ -1,7 +1,10 @@
+# Project version
+VERSION = '1.0.0'
+
 # Default port used by the NTP Exporter
 DEFAULT_PORT = 9951  # TODO: Change this; doesn't have to be official
 
 # Prefix used for the names of all exported metrics
 EXPORTER_PREFIX = 'ntp'
 
-__all__ = [DEFAULT_PORT, EXPORTER_PREFIX]
+__all__ = [DEFAULT_PORT, EXPORTER_PREFIX, VERSION]
